55fb181bdd027271f1b5302388dd5deeccc99d05
caasi Huang <caasi.igd@gmail.com>
2017-02-07 01:08:30 +0800

    Use Material-UI

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,7 +1,11 @@
 import React, { Component, PropTypes } from 'react'
 import cx from 'classnames'
-import JSONTree from 'react-json-tree'
-import me from '../index.json'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton'
+import ActionHome from 'material-ui/svg-icons/action/home'
+import { Card, CardHeader, CardText } from 'material-ui/Card'
+import { grey800 } from 'material-ui/styles/colors'
 
 import styles from './index.css'
 
@@ -21,12 +25,43 @@ class App extends Component {
     const classes = cx(styles.className, 'caasih-app', className)
 
     return (
-      <JSONTree
-        id={id}
-        className={classes}
-        data={me}
-        shouldExpandNode={() => true}
-      />
+      <MuiThemeProvider>
+        <div>
+          <AppBar
+            style={{ background: grey800 }}
+            title="caasih.net"
+            zDepth={0}
+            iconElementLeft={
+              <IconButton onClick={() => location = './'}>
+                <ActionHome />
+              </IconButton>
+            }
+          />
+          <div id="container" style={{ padding: '1rem' }}>
+            <Card style={{ marginBottom: '1rem' }}>
+              <CardHeader
+                title={<a href="http://www.material-ui.com/">Material-UI</a>}
+                subtitle="2017/02/07"
+              />
+              <CardText>
+                試著把 <code>&lt;Card&gt;</code> 當文章用。
+                <br/>效果並不顯著！
+                <br/>
+                <br/>看來得自己刻元件。
+              </CardText>
+            </Card>
+            <Card>
+              <CardHeader
+                title="About"
+                subtitle="2017/02/07"
+              />
+              <CardText>
+                卡西（caasih）是個沒受完 CSIE 訓練的半調子碼農。
+              </CardText>
+            </Card>
+          </div>
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
