11e7b000f6aafeebd844b6456726b37afdb54176
caasi Huang <caasi.igd@gmail.com>
2017-11-19 03:59:24 +0800

    Update posts

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -6,15 +6,20 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import * as actions from 'actions'
 import * as func from 'types/func'
-import { equals } from 'ramda'
+import CreativeCommons from 'components/CreativeCommons'
+import { equals, find, propEq } from 'ramda'
+import moment from 'moment'
 
 import styles from './index.css'
+
+const DATETIME_FORMAT = 'YYYY-MM-DD HH:mm'
 
 
 
 class Post extends PureComponent {
   static propTypes = {
     className: PropTypes.string,
+    meta: PropTypes.object,
     post: PropTypes.string,
   }
 
@@ -36,11 +41,38 @@ class Post extends PureComponent {
   }
 
   render() {
-    const { id, className, post } = this.props
+    const { id, className, meta, post } = this.props
     const classes = cx(styles.className, 'caasih-post', className)
+    let publishedAt
+    let modifiedAt
+
+    if (meta) {
+      publishedAt = moment(meta.datePublished).format(DATETIME_FORMAT)
+      modifiedAt = moment(meta.dateModified).format(DATETIME_FORMAT)
+    }
 
     return (
-      <ReactMarkdown id={id} className={classes} source={post} />
+      <article>
+        {
+          meta &&
+            <ul>
+              <li>發佈於 { publishedAt }</li>
+              {
+                (modifiedAt !== publishedAt) &&
+                  <li>修改於 { modifiedAt }</li>
+              }
+            </ul>
+        }
+        {
+          meta &&
+            <h1>{ meta.headline }</h1>
+        }
+        <ReactMarkdown id={id} className={classes} source={post} />
+        {
+          meta && meta.license &&
+            <CreativeCommons size="compact" {...meta.license} />
+        }
+      </article>
     )
   }
 }
@@ -50,10 +82,11 @@ class Post extends PureComponent {
 export default withRouter(connect(
   (state, router) => {
     const { pid } = router && router.match && router.match.params || {}
-    const { post_list = [] } = state || {}
+    const { post_index = [], post_list = [] } = state || {}
+    const meta = find(propEq('url', pid), post_index)
     const post = post_list[pid]
 
-    return { post }
+    return { meta, post }
   },
   dispatch => ({ actions: func.map(dispatch, actions) })
 )(Post))
