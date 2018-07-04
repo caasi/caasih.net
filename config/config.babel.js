9cf2da0c2e0b72d2cca2900a46d547e0e60fbd8f
caasi Huang <caasi.igd@gmail.com>
2018-07-05 01:59:43 +0800

    Deploy to gh-pages

--- config/config.babel.js
+++ config/config.babel.js
@@ -8,5 +8,5 @@ export const port = process.env.PORT || 3000
 
 export const srcPath = path.resolve(__dirname, '../src')
 
-export const dstPath = path.resolve(__dirname, '../')
+export const dstPath = path.resolve(__dirname, '../dist')
 
