3260f582d1e64d8f9494148d6b005af113b32329
caasi Huang <caasi.igd@gmail.com>
2018-07-14 23:36:34 +0800

    Hide sources by default

---
+++ src/components/SourceCode/index.jsx
@@ -0,0 +1,43 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import cx from 'classnames'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import style from 'react-syntax-highlighter/styles/hljs/github'
+import styles from './index.css'
+
+class SourceCode extends PureComponent {
+  static propTypes = {
+    className: PropTypes.string,
+    lanugage: PropTypes.string,
+  }
+
+  static defaultProps = {
+    className: '',
+  }
+
+  state = {
+    open: false,
+  }
+
+  render() {
+    const { id, className, children, language } = this.props
+    const { open } = this.state
+    const classes = cx(styles.className, 'caasih-source-code', className)
+
+    return (
+      <div id={id} className={classes}>
+        <label onClick={() => this.setState({ open: !open })}>
+          source
+        </label>
+        {
+          open &&
+          <SyntaxHighlighter language={language} style={style}>
+            {children}
+          </SyntaxHighlighter>
+        }
+      </div>
+    )
+  }
+}
+
+export default SourceCode
--- src/pages/Playground/ReactKonva/index.jsx
+++ src/pages/Playground/ReactKonva/index.jsx
@@ -2,9 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 import { Stage, Layer } from 'react-konva'
-import SyntaxHighlighter from 'react-syntax-highlighter'
-import style from 'react-syntax-highlighter/styles/hljs/github'
 import Article from 'components/Article'
+import SourceCode from 'components/SourceCode'
 import CreativeCommons from 'components/CreativeCommons'
 import TextAndRectSource from '!raw-loader!./TextAndRect'
 import TextAndRect from './TextAndRect'
@@ -45,31 +44,31 @@ class ReactKonva extends Component {
         <p>
           乍看很美好，但 Konva.js 不提供像 HTML 或 SVG 那樣神奇的 layout 功能。
         </p>
-        <SyntaxHighlighter language="jsx" style={style}>
-          {TextAndRectSource}
-        </SyntaxHighlighter>
         <MyStage>
           <TextAndRect />
         </MyStage>
+        <SourceCode language="jsx">
+          {TextAndRectSource}
+        </SourceCode>
         <p>
           也就是說，當你想畫個剛好可以容納文字的容器（就叫他 <code>Tag</code> 吧）時，得先把文字畫好，得到其寬高，再決定外框該怎麼畫。沒有什麼文字自己把外框「撐開」這種事。如果想達成此效果，只能等 component render 完畢後，取得文字佔據的長寬，再更新外框。
         </p>
-        <SyntaxHighlighter language="jsx" style={style}>
-          {TextInRectSource}
-        </SyntaxHighlighter>
         <MyStage>
           <TextInRect />
         </MyStage>
+        <SourceCode language="jsx">
+          {TextInRectSource}
+        </SourceCode>
         <p>
           如果今天我希望一列這樣的 <code>Tag</code> ，可以自動抓好彼此間的寬度與間距，該怎麼辦？
           React 16 的 <code>ref</code> 雖然能被<a href="https://reactjs.org/docs/refs-and-the-dom.html#exposing-dom-refs-to-parent-components" title="Exposing DOM Refs to Parent Components">暴露給上層的父組件</a>，但沒辦法讓我們自訂它是什麼，也就無法傳遞我們計算過的寬高。為了讓父組件知道子組件的寬高變化，得另外提供個 callback function ，就叫它 <code>onResize</code> 吧。
         </p>
-        <SyntaxHighlighter language="jsx" style={style}>
-          {TagSource}
-        </SyntaxHighlighter>
         <MyStage>
           <Tag text="a tag" />
         </MyStage>
+        <SourceCode language="jsx">
+          {TagSource}
+        </SourceCode>
         <p>（待續）</p>
         <CreativeCommons size="compact" type="by" />
       </Article>
