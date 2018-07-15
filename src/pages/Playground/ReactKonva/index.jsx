2f59b3781a0eb902437798a9628a74e9291fb370
caasi Huang <caasi.igd@gmail.com>
2018-07-15 21:49:34 +0800

    Update the article style

--- src/pages/Playground/ReactKonva/index.jsx
+++ src/pages/Playground/ReactKonva/index.jsx
@@ -48,18 +48,28 @@ class ReactKonva extends Component {
         <p>
           乍看很美好，但 Konva.js 不提供像 HTML 或 SVG 那樣的排版功能。
         </p>
-        <MyStage>
-          <TextAndRect />
-        </MyStage>
+        <figure>
+          <MyStage>
+            <TextAndRect />
+          </MyStage>
+          <figcaption>
+            <code>Rect</code> 和 <code>Text</code> 彼此無關
+          </figcaption>
+        </figure>
         <SourceCode language="jsx">
           {TextAndRectSource}
         </SourceCode>
         <p>
           也就是說，當你想畫個剛好可以容納文字的容器（就叫他 <code>Tag</code> 吧）時，得先把文字畫好，得到其寬高，再決定外框該怎麼畫。沒有什麼文字自己把外框「撐開」這種事。如果想達成此效果，只能等 component render 完，取得文字佔據的長寬，再更新外框。
         </p>
-        <MyStage>
-          <TextInRect />
-        </MyStage>
+        <figure>
+          <MyStage>
+            <TextInRect />
+          </MyStage>
+          <figcaption>
+            <code>Text</code> 畫完後，再根據其長寬畫外框
+          </figcaption>
+        </figure>
         <SourceCode language="jsx">
           {TextInRectSource}
         </SourceCode>
@@ -67,32 +77,47 @@ class ReactKonva extends Component {
           如果今天希望一列這樣的 <code>Tag</code> ，可以自動抓好彼此間的寬度與間距，該怎麼辦？
           React 16 的 <code>ref</code> 雖然能被<a href="https://reactjs.org/docs/refs-and-the-dom.html#exposing-dom-refs-to-parent-components" title="Exposing DOM Refs to Parent Components">暴露給上層的父組件</a>，但沒辦法讓我們自訂它是什麼，也就無法傳遞我們計算過的寬高。為了讓父組件知道子組件的寬高變化，得另外提供個 callback function ，就叫它 <code>onResize</code> 吧。
         </p>
-        <MyStage>
-          <Tag text="a tag" />
-        </MyStage>
+        <figure>
+          <MyStage>
+            <Tag text="a tag" />
+          </MyStage>
+          <figcaption>
+            一個 <code>Tag</code> 長這樣
+          </figcaption>
+        </figure>
         <SourceCode language="jsx" label="Tag.jsx">
           {TagSource}
         </SourceCode>
         <p>接著準備一個容器 <code>TagList</code> ，他會記下子組件送來的寬度，好重新排版。將子組件當成 <code>children</code> 傳遞給它時，不用提供 <code>onResize</code> ，由 <code>TagList</code> 自己以 <code>cloneElement</code> 插入。</p>
-        <MyStage>
-          <TagList>
-            <Tag text="this" />
-            <Tag text="is" />
-            <Tag text="my" />
-            <Tag text="tags" />
-          </TagList>
-        </MyStage>
+        <figure>
+          <MyStage>
+            <TagList>
+              <Tag text="this" />
+              <Tag text="is" />
+              <Tag text="my" />
+              <Tag text="tags" />
+            </TagList>
+          </MyStage>
+          <figcaption>
+            以迴圈實作的 <code>TagList</code>
+          </figcaption>
+        </figure>
         <SourceCode language="jsx" label="TagList.jsx">
           {TagListSource}
         </SourceCode>
         <p>也可以遞迴實作這個 <code>TagList</code> 。</p>
-        <MyStage>
-          <TagListRec>
-            <Tag text="這是" />
-            <Tag text="一堆" />
-            <Tag text="標籤" />
-          </TagListRec>
-        </MyStage>
+        <figure>
+          <MyStage>
+            <TagListRec>
+              <Tag text="這是" />
+              <Tag text="一堆" />
+              <Tag text="標籤" />
+            </TagListRec>
+          </MyStage>
+          <figcaption>
+            遞迴實作的 <code>TagList</code>
+          </figcaption>
+        </figure>
         <SourceCode language="jsx" label="TagListRec.jsx">
           {TagListRecSource}
         </SourceCode>
