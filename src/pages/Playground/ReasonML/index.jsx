3ddb9aec6fceeeea834e2cfc964169c70b47b284
caasi Huang <caasi.igd@gmail.com>
2020-02-28 02:09:50 +0800

    Add the missing picture license

--- src/pages/Playground/ReasonML/index.jsx
+++ src/pages/Playground/ReasonML/index.jsx
@@ -31,6 +31,9 @@ class PureScript extends React.PureComponent<Props> {
         <h1>ReasonML</h1>
         <p>參考 <a href="https://dev.to/yakimych/adding-reasonml-to-an-existing-codebase-part-1-3kkg">Adding ReasonML to an existing codebase (Part 1)</a>，現在除了 PureScript 外，還可以同時以 OCaml/ReasonML 寫 React 元件 XD</p>
         <ImageData />
+        <div style={{ textAlign: 'center', color: '#666' }}>
+          <a href="https://www.flickr.com/photos/84175980@N00/6595168855">"20111227 nigel"</a> by <a href="https://www.flickr.com/photos/84175980@N00">schizoform</a> is licensed under <a href="https://creativecommons.org/licenses/by/2.0"> CC BY 2.0 </a>
+        </div>
         <SourceCode open language="reasonml">
           {ImageDataSource}
         </SourceCode>
