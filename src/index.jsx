e2d6ee60d503c0ef1ee7f20e6fba18c06c21d379
caasi Huang <caasi.igd@gmail.com>
2023-02-27 23:56:55 +0800

    feat: use 漢字標準格式 and update color scheme

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { render } from 'react-snapshot'
 import Root from './Root'
 
-import 'normalize.css/normalize.css'
 import './index.css'
 
 const rootEl = document.getElementById('app')
